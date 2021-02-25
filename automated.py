7851ab69c41db374a1870f7b9f5c091d9687f6af
Huong Le <hle@pmhcc.org>
2021-02-25 11:24:23 -0500

    aaa bugs

--- automated.py
+++ automated.py
@@ -3,6 +3,7 @@ import os
 import glob
 import pandas as pd
 from pandas import DataFrame 
+import re
 import numpy as np
 import sqlite3
 import pyodbc
@@ -21,22 +22,10 @@ def connect_to_db():
 
 
 
-def all_admits_queries():
-	"""create queries for admits in list of facilities"""
-	base_query = """SELECT [fldHousingEventID#]
-	,clt.FirstName
-	,clt.LastName
-	,typ.[fldHousingLocationID#]
-	,typ.FldHousingName
-	,[fldEntrydateofhousing]
-	,[fldDischargeDate/Max-out]
-	,[CurrentEvent]
-FROM [SCCJU].[dbo].[tblHousingEvents] as evnt
-JOIN tblHousingType as typ ON evnt.fldHousingLocationID# = typ.fldHousingLocationID#
-JOIN tblClient as clt ON evnt.fldClientID# = clt.ClientID#"""
-	flist = ["Girard Recovery Center", "Horizon House - Old York DBT", "Gaudenzia RTFA", "Gaudenzia RTFA (BHJRS)", "New Vitae - South", "New Vitae - South (Non_ACLU)", "New Vitae - West I", "New Vitae - West II", "New Vitae - West I (Non-ACLU)", "New Vitae - West II (Non-ACLU)", "VOA - Roosevelt TBI", "VOA - Upsal TBI"]
+def all_admits_queries(base_query, facility_list):
+	"""create queries for admits in list of facilities"""	
 	admits_queries = []
-	for facil in flist:
+	for facil in facility_list:
 		admit_query = base_query + f"""\nWHERE FldHousingName LIKE '%{facil}'
 AND [fldDischargeDate/Max-out] IS NULL
 ORDER BY fldEntrydateofhousing DESC"""
@@ -45,22 +34,10 @@ ORDER BY fldEntrydateofhousing DESC"""
 
 
 
-def all_discharges_queries():
+def all_discharges_queries(base_query, facility_list):
 	"""create queries for discharges in list of facilities"""
-	base_query = """SELECT [fldHousingEventID#]
-	,clt.FirstName
-	,clt.LastName
-	,typ.[fldHousingLocationID#]
-	,typ.FldHousingName
-	,[fldEntrydateofhousing]
-	,[fldDischargeDate/Max-out]
-	,[CurrentEvent]
-FROM [SCCJU].[dbo].[tblHousingEvents] as evnt
-JOIN tblHousingType as typ ON evnt.fldHousingLocationID# = typ.fldHousingLocationID#
-JOIN tblClient as clt ON evnt.fldClientID# = clt.ClientID#"""
-	flist = ["Girard Recovery Center", "Horizon House - Old York DBT", "Gaudenzia RTFA", "Gaudenzia RTFA (BHJRS)", "New Vitae - South", "New Vitae - South (Non_ACLU)", "New Vitae - West I", "New Vitae - West II", "New Vitae - West I (Non-ACLU)", "New Vitae - West II (Non-ACLU)", "VOA - Roosevelt TBI", "VOA - Upsal TBI"]
 	disc_queries = []
-	for facil in flist:
+	for facil in facility_list:
 		disc_query = base_query + f"""\nWHERE FldHousingName LIKE '%{facil}'
 ORDER BY [fldDischargeDate/Max-out] DESC"""
 		disc_queries.append(disc_query)
@@ -118,22 +95,32 @@ def get_latest_sheet(wd):
 	print(f"taken from {latest_sheet}\n\n\n")
 	sheet_df = pd.read_excel(latest_sheet,sheet_name="Individual Data")
 	sheet_df.columns = ["Status","EntryDate","HousingName","SSN","FirstName","LastName","DOB","PriorHousing","PriorEntryDate","PriorDischargeDate","DischargeDate","DischargeReason","ResidenceDischargedTo"]
-	return sheet_df
+	bed_list_df = pd.read_excel(latest_sheet,sheet_name="Available County Beds")
+	return sheet_df, bed_list_df
+
+def split_bed_list(bed_list_df, facility_list):
+	bed_list_df = bed_list_df["Community/Supportive Slot"]
+	bed_list_df = bed_list_df.to_frame()
+	bed_list_df["HousingName"] = bed_list_df["Community/Supportive Slot"].str.extract(r"(.+)\s\d+")
+	bed_list_df["BedNumber"] = bed_list_df["Community/Supportive Slot"].str.extract(r"(\d+)")
+	print(bed_list_df)
+
+	bed_list_df = bed_list_df[bed_list_df["HousingName"].isin(facility_list)]
+	
+	print(bed_list_df)
+	print(bed_list_df.dtypes)
 
 
 def cross_check_admits(all_admits,sheet_df):
 	all_admits = all_admits.assign(UpdateStatus = (all_admits.FirstName.isin(sheet_df.FirstName)) & (all_admits.LastName.isin(sheet_df.LastName)) & (all_admits.EntryDate.isin(sheet_df.EntryDate)))
-	return all_admits
-
-
+	not_updated_df = all_admits.loc[all_admits["UpdateStatus"]==False]
+	return not_updated_df
 
 def cross_check_discharges(all_discharges,sheet_df):
 	all_discharges = all_discharges.assign(UpdateStatus = (all_discharges.FirstName.isin(sheet_df.FirstName)) & (all_discharges.LastName.isin(sheet_df.LastName)) & (all_discharges.EntryDate.isin(sheet_df.EntryDate)) & (all_discharges.DischargeDate.isin(sheet_df.DischargeDate)))
-	return all_discharges
-
-def entries_not_updated(df):
-	not_updated_df = df.loc[df["UpdateStatus"]==False]
+	not_updated_df = all_discharges.loc[all_discharges["UpdateStatus"]==False]
 	return not_updated_df
+
 
 
 
@@ -141,32 +128,39 @@ def main():
 	"""main function to queue other functions"""
 	###create cursor
 	cs = connect_to_db()
-
+	facility_list = ["Girard Recovery Center", "Horizon House - Old York DBT", "Gaudenzia RTFA", "Gaudenzia RTFA (BHJRS)", "New Vitae - South", "New Vitae - South (Non_ACLU)", "New Vitae - West I", "New Vitae - West II", "New Vitae - West I (Non-ACLU)", "New Vitae - West II (Non-ACLU)", "VOA - Roosevelt TBI", "VOA - Upsal TBI"]
+	
+	base_query = """SELECT [fldHousingEventID#]
+	,clt.FirstName
+	,clt.LastName
+	,typ.[fldHousingLocationID#]
+	,typ.FldHousingName
+	,[fldEntrydateofhousing]
+	,[fldDischargeDate/Max-out]
+	,[CurrentEvent]
+FROM [SCCJU].[dbo].[tblHousingEvents] as evnt
+JOIN tblHousingType as typ ON evnt.fldHousingLocationID# = typ.fldHousingLocationID#
+JOIN tblClient as clt ON evnt.fldClientID# = clt.ClientID#"""
+	
 	### export & reformat dataframe of all admits
-	admits_queries = all_admits_queries()
+	admits_queries = all_admits_queries(base_query, facility_list)
 	all_admits_df = execute_query(cs, admits_queries)
-	all_admits = reformat_df(all_admits_df,type="admits")	
+	all_admits_df = reformat_df(all_admits_df, type="admits")	
 
 	### export & reformat dataframe of al discharges
-	disc_queries = all_discharges_queries()
-	all_disc_df = execute_query(cs, disc_queries)
-	all_discharges = reformat_df(all_disc_df,type="discharges")
+	disc_queries = all_discharges_queries(base_query, facility_list)
+	all_discharges_df = execute_query(cs, disc_queries)
+	all_discharges_df = reformat_df(all_discharges_df, type="discharges")
 
 	### convert latest sheet to dataframe
 	wd = set_dir()
-	sheet_df = get_latest_sheet(wd)
+	sheet_df, bed_list_df = get_latest_sheet(wd)
 
-	all_admits = cross_check_admits(all_admits,sheet_df)
-	all_discharges = cross_check_discharges(all_discharges,sheet_df)
+	not_updated_df = cross_check_admits(all_admits_df,sheet_df)
+	not_updated_df = not_updated_df.append(cross_check_discharges(all_discharges_df,sheet_df))
+	#print(not_updated_df)
 
-	not_updated_admits = entries_not_updated(all_admits)
-	not_updated_discharges = entries_not_updated(all_discharges)
-	print(not_updated_admits)
-	print(not_updated_discharges)
-
-
-
-
+	bed_list_df = split_bed_list(bed_list_df, facility_list)
 
 
 
